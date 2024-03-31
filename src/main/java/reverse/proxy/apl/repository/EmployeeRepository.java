27cebd30ee5710f52d78bad2414a99f758b53309
TsukasaHirai <www.abc.jda.yasu@gmail.com>
2024-04-01 00:08:13 +0900

    型変更

--- src/main/java/reverse/proxy/apl/repository/EmployeeRepository.java
+++ src/main/java/reverse/proxy/apl/repository/EmployeeRepository.java
@@ -1,14 +1,13 @@
 package reverse.proxy.apl.repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 import com.google.cloud.spring.data.spanner.repository.SpannerRepository;
 
 import reverse.proxy.apl.dto.Employee;
 
 
-public interface EmployeeRepository extends SpannerRepository<Employee, UUID>{
+public interface EmployeeRepository extends SpannerRepository<Employee, String>{
 	
 	Optional<Employee> findByUsername(String username);
 }
